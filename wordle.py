a65e4e2e0e9e62bcea59bd5bfd1ff56e87405d03
rdelaf <82660085+rdelaf@users.noreply.github.com>
2022-03-07 09:36:20 -0100

    Checkpoint2

--- wordle.py
+++ wordle.py
@@ -1,5 +1,6 @@
 from fileinput import filename
 import random
+import string
 
 
 def choose_secret(filename):
@@ -10,11 +11,12 @@ def choose_secret(filename):
       secret: Palabra elegida aleatoriamente del fichero transformada a mayúsculas. Ej. "CREMA"
     """
     f = open(filename)
-    secret = f.read(random.randint(0, 31))
+    print(random.randint(0, 31))
+    secret = f.readline(random.randint(0, 31))
     print(secret)
     return secret
     
-def compare_words():
+def compare_words(word, secret):
     """Dadas dos palabras en mayúsculas (word y secret), esta función calcula las posiciones de las letras de word que aparecen en la misma posición en secret, y las posiciones de las letras de word que aparecen en secret pero en una posición distinta.
     Args:
       word: Una palabra. Ej. "CAMPO"
@@ -24,7 +26,20 @@ def compare_words():
       same_letter: Lista de posiciones de word cuyas letras están en secret pero en posiciones distintas. En el caso anterior: [1,2]
     """
 
-def print_word():
+    same_position = []
+    same_letter = []
+
+    for i in range(len(word)):
+      for j in range(secret):
+        if word[i] == secret[j]:
+          if i == j:
+            same_position.append(i)
+          else:
+            same_letter.append(i)
+    
+    return same_position, same_letter
+
+def print_word(word, same_letter_position, same_letter):
     """Dada una palabra, una lista same_position y otra lista same_letter, esta función creará un string donde aparezcan en mayúsculas las letras de la palabra que ocupen las posiciones de same_position, en minúsculas las letras de la palabra que ocupen las posiciones de same_letter y un guión (-) en el resto de posiciones
     Args:
       word: Una palabra. Ej. "CAMPO"
@@ -33,6 +48,18 @@ def print_word():
     Returns:
       transformed: La palabra aplicando las transformaciones. En el caso anterior: "Cam--"
     """
+
+    #for i in range(word):
+
+
+    for i in range(len(same_letter_position)):
+      word.replace(word[same_letter_position[i]], word[same_letter_position[i]].isupper())
+
+    for i in range(len(same_position)):
+      word.replace(word[same_letter_position[i]], word[same_letter_position[i]].lower())
+
+    
+
     
 def choose_secret_advanced():
     """Dado un nombre de fichero, esta función filtra solo las palabras de 5 letras que no tienen acentos (á,é,í,ó,ú). De estas palabras, la función devuelve una lista de 15 aleatorias no repetidas y una de estas 15, se selecciona aleatoriamente como palabra secret.
